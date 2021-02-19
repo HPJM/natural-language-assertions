c744362d84d8bb822d4dd1d49e21369803f63db4
Hugo Morgan <hpjm37@gmail.com>
2021-02-19 15:20:44 +0000

    add eq and between, update readme

--- README.md
+++ README.md
@@ -2,31 +2,51 @@
 
 This project is meant to add natural language assertions so that testing can be done in plain English.
 
+## Supported syntax
+
+### Between
+- `x is between y and z`
+- `x should be between y and z`
+
+### Equality (`y` can be literal or binding)
+- `x is y`
+- `x equals y`
+- `x should be y`
+- `x should equal y`
+
+### Containing (`y` can be literal or binding)
+- `x contains y`
+- `x includes y`
+- `x has y`
+- `x should contain y`
+- `x should include y`
+- `x should have y`
 ## Examples
 
 ```elixir
 defmodule Example do
   use NaturalLanguageAssertions
 
-  assert_natural list: [1, 2, 3] do
-    list should contain 2
-    list contains 2
-    list should contain 100
-    list contains 100
-  end
-
-  assert_natural collection: ["dog", "cat"] do
-    collection includes 3
-    collection should include 3
-    collection includes "dog"
-    collection should include "cat"
+  assert_natural answer: 4 do
+    answer is 4
+    answer should equal 4
+    answer should be between 3 and 5
   end
 
   assert_natural collection: [:hello, :world] do
     collection has :hello
-    collection should have :hello
-    collection has :yellow
-    collection should have :yellow
+    collection should include :hello
+    collection should contain :world
+  end
+
+  assert_natural list: [:hello, :world], planet: :world, animal: :cat do
+    # should fail: non-existent binding
+    something has planet
+    list has planet
+    # should fail: non-existent binding
+    list includes dog
+    # should fail: not in list
+    list should contain animal
   end
 
   def results do
--- lib/natural_language_assertions.ex
+++ lib/natural_language_assertions.ex
@@ -1,19 +1,39 @@
 defmodule NaturalLanguageAssertions do
   @list_verbs_plural [:includes, :contains, :has]
   @list_verbs_singular [:include, :contain, :have]
-
-  @verbs @list_verbs_plural ++ @list_verbs_singular
+  @equality_verbs [:is, :be, :equal, :equals, :resemble, :resembles]
+  @list_verbs @list_verbs_plural ++ @list_verbs_singular
+  @verbs @list_verbs ++ @equality_verbs
 
   def verb_label(verb, value, :ok), do: "#{verb} #{inspect(value)}"
   def verb_label(verb, value, :fail), do: "doesn't #{verb} #{inspect(value)}"
 
-  defp list_label_prefix(list, key) do
-    "#{key} #{inspect(list)}" <> " "
-  end
-  defp get_label(list, key, verb, value, status) do
-    list_label_prefix(list, key) <> get_list_label(verb, value, status)
+  def valid_verb?(verb) do
+    verb in @verbs
   end
 
+  defp label_prefix(value, key) do
+    "#{key} #{inspect(value)}" <> " "
+  end
+  defp list_label(list, key, verb, value, status) do
+    label_prefix(list, key) <> get_list_label(verb, value, status)
+  end
+  defp eq_label(lhs, key, :equal, value, status) do
+    eq_label(lhs, key, :equals, value, status)
+  end
+  defp eq_label(lhs, key, :be, value, status) do
+    eq_label(lhs, key, :is, value, status)
+  end
+  defp eq_label(lhs, key, verb, value, status) do
+    label_prefix(lhs, key) <> verb_label(verb, value, status)
+  end
+
+  defp between_label(lhs, key, low, high, :fail) do
+    label_prefix(lhs, key) <> "is not between #{low} and #{high}"
+  end
+  defp between_label(lhs, key, low, high, :ok) do
+    label_prefix(lhs, key) <> "is between #{low} and #{high}"
+  end
 
   for {singular, plural} <- Enum.zip(@list_verbs_singular, @list_verbs_plural) do
     defp get_list_label(unquote(singular), value, :ok) do
@@ -56,7 +76,31 @@ defmodule NaturalLanguageAssertions do
     collection = bindings[key] || []
     result? = value in collection
     status = if result?, do: :ok, else: :fail
-    label = get_label(collection, key, verb, value, status)
+    label = list_label(collection, key, verb, value, status)
+
+    quote bind_quoted: [value: value, label: label, status: status], unquote: true do
+      @results {status, label}
+      {status, label}
+    end
+  end
+
+  defp assert_eq(bindings, key, verb, value) do
+    value = if is_atom(value), do: Keyword.get(bindings, value, value), else: value
+    result? = value == bindings[key]
+    status = if result?, do: :ok, else: :fail
+    label = eq_label(bindings[key], key, verb, value, status)
+
+    quote bind_quoted: [value: value, label: label, status: status], unquote: true do
+      @results {status, label}
+      {status, label}
+    end
+  end
+
+  defp assert_between(bindings, key, [low, high]) do
+    value = bindings[key]
+    result? = value > low and value < high
+    status = if result?, do: :ok, else: :fail
+    label = between_label(bindings[key], key, low, high, status)
 
     quote bind_quoted: [value: value, label: label, status: status], unquote: true do
       @results {status, label}
@@ -68,19 +112,41 @@ defmodule NaturalLanguageAssertions do
     Macro.prewalk(block, &prewalk(&1, bindings))
   end
 
-  defp prewalk({binding_key, _, [{:should, _, [{verb, _, [{value, _, _}]}]}]}, bindings) when verb in @verbs do
+  defp prewalk({:the, _meta, inner}, bindings), do: prewalk(inner, bindings)
+
+  defp prewalk({binding_key, _, [{:should, _, [{verb, _, [{:between, _, [{:and, _, values}]}]}]}]}, bindings) when verb in @equality_verbs do
+    assert_between(bindings, binding_key, values)
+  end
+
+  defp prewalk({binding_key, _, [{:should, _, [{verb, _, [value]}]}]}, bindings) when verb in @equality_verbs do
+    assert_eq(bindings, binding_key, verb, value)
+  end
+
+  defp prewalk({binding_key, _, [{verb, _, [{:between, _, [{:and, _, values}]}]}]}, bindings) when verb in @equality_verbs do
+    assert_between(bindings, binding_key, values)
+  end
+
+  defp prewalk({binding_key, _, [{verb, _, [{value, _, _}]}]}, bindings) when verb in @equality_verbs do
+    assert_eq(bindings, binding_key, verb, value)
+  end
+
+  defp prewalk({binding_key, _, [{verb, _, [value]}]}, bindings) when verb in @equality_verbs do
+    assert_eq(bindings, binding_key, verb, value)
+  end
+
+  defp prewalk({binding_key, _, [{:should, _, [{verb, _, [{value, _, _}]}]}]}, bindings) when verb in @list_verbs do
     assert_in(bindings, binding_key, verb, value)
   end
 
-  defp prewalk({binding_key, _, [{:should, _, [{verb, _, [value]}]}]}, bindings) when verb in @verbs do
+  defp prewalk({binding_key, _, [{:should, _, [{verb, _, [value]}]}]}, bindings) when verb in @list_verbs do
     assert_in(bindings, binding_key, verb, value)
   end
 
-  defp prewalk({binding_key, _, [{verb, _, [{value, _, _}]}]}, bindings) when verb in @verbs do
+  defp prewalk({binding_key, _, [{verb, _, [{value, _, _}]}]}, bindings) when verb in @list_verbs do
     assert_in(bindings, binding_key, verb, value)
   end
 
-  defp prewalk({binding_key, _, [{verb, _, [value]}]}, bindings) when verb in @verbs do
+  defp prewalk({binding_key, _, [{verb, _, [value]}]}, bindings) when verb in @list_verbs do
     assert_in(bindings, binding_key, verb, value)
   end
 
